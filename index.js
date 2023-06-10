047872b056e7adb0a84d91088a6e92455503d2cb
Steven Rosario <strosariogarcia@gmail.com>
2023-06-10 18:26:22 -0400

    Added merge sort

--- index.js
+++ index.js
@@ -69,3 +69,70 @@ function insertionSort(arr){
 
 
 insertionSort([2,1,9,12,4])
+
+// Faster family of sorting algorithms: Megrge, Quick, Radix
+// Time complexity -> O(n log n)
+
+//Merge Sort: combination of merging and sorting
+// Divide and Conquer approach (split the larger array into subarrays of 0 or 1 elements) 
+
+// Helper merge function:
+// It is useful to first implement a function that merges two sorted array
+// This helper function creates a new array which is sorted and consists of all of the elements in the two input arrays
+// This function should run in O(n+m) time and space
+// Create an empty array
+// Recommendation: while loop (while there are still values we have not looked)
+// If the value in the first array is smaller than the value in the second array, push the value in the first array into our results and move on to the next value in the first array
+// If the value in the first array is larger than the value in the second array, push the value in the second array into our results and move on to the next value in the second array
+
+function merge(arr1, arr2){
+  let results = [];
+  let i = 0;
+  let j = 0;
+
+  while(i < arr1.length && j < arr2.length){
+    if(arr2[j] > arr1[i]){
+      results.push(arr1[i])
+      i++
+    } else {
+      results.push(arr2[j])
+      j++
+    }
+  }
+
+  while(i < arr1.length){
+    results.push(arr1[i])
+    i++
+  }
+  while(j < arr2.length){
+    results.push(arr2[j])
+    j++
+  }
+  
+  return results;
+}
+
+// merge([1,10,50], [2,14,99,100])
+//        i          j
+// results [1, 2]
+
+// mergeSort Psuedocode:
+// mergeSort is recursive
+// Big O: O(n log n)
+// O(log n) decompositions (breaking the arrays into halves)
+// O(n) comparisons per decomposition (we compare the elements)
+// Break up the array into halves until the arrays are empty or have one element
+// Once we have the smaller sorted arrays, merge those arrays with other sorted arrays until you are back at the full length
+// Return the merged and sorted array
+function mergeSort(arr){
+  if(arr.length <= 1) return arr;
+  // recommend slice the array to split the array into halves
+  // arr.slice(0,Math.floor(arr.length/2))
+  let mid = Math.floor(arr.length/2)
+  let left = mergeSort(arr.slice(0,mid))
+  let right = mergeSort(arr.slice(mid))
+  return merge(left,right)
+  
+}
+
+console.log(mergeSort([10,24,76,73,1]))
